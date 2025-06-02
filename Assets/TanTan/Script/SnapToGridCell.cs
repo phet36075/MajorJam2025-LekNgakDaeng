728daa685be56a2cad820e4cb6b94a18f2f9cfea
Tan20114 <156587199+Tan20114@users.noreply.github.com>
2025-06-02 22:36:31 +0700

    SnapToGridCell Script

---
+++ Assets/TanTan/Script/SnapToGridCell.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SnapToGridCell : MonoBehaviour
+{
+    [Header("Coordinate")]
+    [SerializeField] Vector2 targetPos;
+
+    void Start()
+    {
+        float tmp = 1000000;
+        CoordScript cs = null;
+
+        foreach (CoordScript c in GridManager.coord)
+        {
+            float dis = Vector2.Distance(transform.position, c.transform.position);
+
+            if (tmp > dis)
+            {
+                tmp = dis;
+                cs = c;
+                targetPos = c.transform.position;
+            }
+        }
+        transform.position = targetPos;
+    }
+}
---
+++ Assets/TanTan/Script/SnapToGridCellAgent.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SnapToGridCellAgent : MonoBehaviour
+{
+    NavMeshAgent agent => GetComponent<NavMeshAgent>();
+
+    [Header("Coordinate")]
+    [SerializeField] Vector2 targetPos = Vector2.zero;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+        float tmp = 1000000;
+
+        foreach (CoordScript c in GridManager.coord)
+        {
+            float dis = Vector2.Distance(transform.position, c.transform.position);
+
+            if (tmp > dis)
+            {
+                tmp = dis;
+                targetPos = c.transform.position;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(targetPos);
+    }
+}
