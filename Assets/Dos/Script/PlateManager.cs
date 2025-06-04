13b04a72dfdfc737b5859ffd3e6a6749b8003dcb
xFallerz <arromdeedos@gmail.com>
2025-06-05 03:44:26 +0700

    3 level done

--- Assets/Dos/Script/PlateManager.cs
+++ Assets/Dos/Script/PlateManager.cs
@@ -1,16 +1,26 @@
 using System.Collections.Generic;
 using UnityEngine;
+using NavMeshPlus.Components;
 
 public class PlateManager : MonoBehaviour
 {
     public List<PlateChecker> plateCheckers = new List<PlateChecker>();
+
+    [Header("Level 5")]
     public GameObject objToSpawn;
+    public Transform spawnPos;
+
+    [Header("Level 15")]
+    public GameObject objToRemove;
+    [SerializeField] private Level curLevel;
+
+    private NavMeshSurface surface => FindAnyObjectByType<NavMeshSurface>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-
+    bool isSpawn;
     // Update is called once per frame
     void Update()
     {
@@ -20,10 +30,33 @@ public class PlateManager : MonoBehaviour
         {
             if(checker.isOn)
                 checkCount++;
-            if (checkCount == amountOfPlate)
+            if (checkCount == amountOfPlate && !isSpawn)
             {
-                Debug.Log("Yay");
+                Level5();
+                Level14();
+                isSpawn = true;
             }
         }
+    }
+
+    private void Level14()
+    {
+        if (curLevel == Level.Level14)
+            Destroy(objToRemove);
+        surface.BuildNavMesh();
+    }
+
+    private void Level5()
+    {
+        if (curLevel == Level.Level5)
+        {
+            Instantiate(objToSpawn, spawnPos.position, Quaternion.identity);
+        }
+    }
+
+    enum Level
+    {
+        Level5,
+        Level14
     }
 }
