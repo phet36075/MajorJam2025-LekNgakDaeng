fafd909b1be761d95369b53f480dfde6f1f9bcad
Tan20114 <156587199+Tan20114@users.noreply.github.com>
2025-06-04 19:59:10 +0700

    Reload Fix

--- Assets/TanTan/Script/Singleton.cs
+++ Assets/TanTan/Script/Singleton.cs
@@ -25,7 +25,7 @@ public class Singleton<T> : MonoBehaviour where T : Component
                 if (_instance == null)
                 {
                     GameObject obj = new GameObject();
-                    obj.hideFlags = HideFlags.HideAndDontSave;
+                    //obj.hideFlags = HideFlags.HideAndDontSave;
                     _instance = obj.AddComponent<T>();
                 }
             }
