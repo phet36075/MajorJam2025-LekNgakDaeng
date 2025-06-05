ad9f55ca538ad9831be77038673ba7747bf3650b
phet36075 <phet36075@hotmail.com>
2025-06-05 21:49:00 +0700

    level fix

---
+++ Assets/ExitScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ExitScript : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
