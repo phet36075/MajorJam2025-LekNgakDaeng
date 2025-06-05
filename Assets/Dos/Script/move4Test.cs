3703cae97de86fef970ba78de0008364036dabe1
xFallerz <arromdeedos@gmail.com>
2025-06-05 19:59:58 +0700

    Last Level Fixed

--- Assets/Dos/Script/move4Test.cs
+++ Assets/Dos/Script/move4Test.cs
@@ -4,15 +4,36 @@ public class move4Test : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D rb;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public bool isLeft;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed;
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        rb.linearVelocity = new Vector2(moveX, moveY) * speed;
+
+        // Flip the sprite based on direction
+        if (moveX < 0 && !isLeft)
+        {
+            Flip(true);
+        }
+        else if (moveX > 0 && isLeft)
+        {
+            Flip(false);
+        }
+    }
+
+    void Flip(bool faceLeft)
+    {
+        isLeft = faceLeft;
+        Vector3 scale = transform.localScale;
+        scale.x = Mathf.Abs(scale.x) * (isLeft ? -1 : 1);
+        transform.localScale = scale;
     }
 }
